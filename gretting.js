75d8ad6f22b81c1122c8acc1482666da14fe83e3
jewanchoi0325 <celebrate1231@gmail.com>
2020-02-27 12:01:21 +0900

    200227edit

--- gretting.js
+++ gretting.js
@@ -1,9 +1,8 @@
-const form = document.querySelector(".js-form");
-const input = form.querySelector("input");
-const greeting = document.querySelector(".js-greetings");
-
-const USER_LS = "currentUser";
-const SHOWING_CN = "showing";
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
 
 function saveName(text) {
   localStorage.setItem(USER_LS, text);
@@ -18,15 +17,14 @@ function handleSubmit(event) {
 
 function askForName() {
   form.classList.add(SHOWING_CN);
-  form.addEventListener("submit", "handleSubmit");
+  form.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
-  greeting.innertext = `Hello ${text}`;
+  greeting.innerText = `Hello ${text}`;
 }
-
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
@@ -35,7 +33,6 @@ function loadName() {
     paintGreeting(currentUser);
   }
 }
-
 function init() {
   loadName();
 }
---
+++ weather.js
@@ -0,0 +1,35 @@
+const CORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("can not access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCords = localStorage.getItem(COORDS);
+  if (loadCoords === null) {
+    askForCoords();
+  } else {
+  }
+}
+
+function init() {}
+
+init();
